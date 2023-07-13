b2cc4666c7d67a1774587176a8f682b3265fe030
saadsamir <sa3dolaa2002@gmail.com>
2023-07-13 16:07:21 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
-#include <unistd.h>
+#include<unistd.h>
 
-/***/
+/**
+ * main - Entry point
+ *
+ * Return: 1 (error)
+ */
 int main(void)
 {
 	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1.a.59);
+	write(1. a. 59);
 	return (1);
+}
