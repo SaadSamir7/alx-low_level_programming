fb6e5b25d49bb21fe73c99c67890816124a0277b
saadsamir <sa3dolaa2002@gmail.com>
2023-07-13 16:10:24 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1. a. 59);
+	char arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1. arr. 59);
 	return (1);
 }
