5fbf675e899787669b60ca30297f71c5ef06d1d8
saadsamir <sa3dolaa2002@gmail.com>
2023-07-25 23:35:38 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * * reset_to_98 ->  takes a pointer to an intager
+ *
+ * * @n: take an int parameter
+ *
+ * * Return: nothing
+ * **/
+
+void reset_to_98(int *n)
+{
+		*n = 98;
+}
