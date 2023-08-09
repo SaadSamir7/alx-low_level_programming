660be153ab1f8396bf684ca0d3b1169c1fad5bae
saadsamir <sa3dolaa2002@gmail.com>
2023-08-09 05:07:14 +0300

    malloc-free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,64 +1,43 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * strtow - concatenates all the arguments of your program
- *@str: string
- *@av: arguments
- * Return: a pointer to a new string
- */
+* **strtow - function that splits a string into words.
+* @str : pointer
+* Return: Char
+*/
+
 char **strtow(char *str)
 {
-	int i, w, j, k, count, m, wordf;
-	char **p;
-	char *x;
+	char **d;
+	int i;
+	int j = 0;
+	int con = 0;
 
-	w = 0;
-	j = 0;
-	i = 0;
-	count = 0;
-	if (*str == '\0' || str == 0)
+	if (str == 0)
 		return (0);
-	for (i = 0; str[i] != '\0'; i++)
+
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
-			w++;
+		if (str[i] != 32)
+			con++;
 	}
-	p = (char **)malloc((w + 1) * sizeof(char *));
-	if (p == 0)
+
+	d = malloc(sizeof(char) * con);
+
+	if (d == 0)
 		return (0);
-	for (wordf = 0; str[wordf] && j <= w; wordf++)
+
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		count = 0;
-		if (str[wordf] != ' ')
+		if (str[i] != 32)
 		{
-			for (i = wordf ; str[i] != '\0'; i++)
-			{
-				if (str[i] == ' ')
-					break;
-				count++;
-			}
-			*(p + j) = (char *)malloc((count + 1) * sizeof(char));
-			if (*(p + j) == 0)
-			{
-				for (k = 0; k <= j; k++)
-				{
-					x = p[k];
-					free(x);
-				}
-				free(p);
-				return (0);
-			}
-			for (m = 0; wordf < i; wordf++)
-			{
-				p[j][m] = str[wordf];
-				m++;
-			}
-			p[j][m] = '\0';
+			*d[j] = str[i];
 			j++;
 		}
+		else
+		{
+		}
 	}
-	p[j] = 0;
-	return (p);
+	return (d);
 }
