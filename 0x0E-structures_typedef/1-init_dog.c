866b03b43089e770e39b898d6a22b07c59c7b717
SaadSamir7 <132520150+SaadSamir7@users.noreply.github.com>
2023-08-16 02:52:06 +0300

    struct-typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,18 +1,21 @@
-#include <stdlib.h>
 #include "dog.h"
 
 /**
- * init_dog - variable of type struct dog
- * @d: pointer to struct dog
- * @name: name
- * @age: age
- * @owner: owner
+ *init_dog - a dog's basic info
+ *@d: dog
+ *@name: dog name
+ *@age: dog age
+ *@owner: owner name
+ *
+ *Return: void
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,26 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
- *
- * Description: dog struct only
- */
-struct dog
+* struct dog - dog
+* @name: name of dog
+* @age: age of dog
+* @owner: owner dogs
+* Description: Defines metadata about the dog and type of dogs
+**/
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
