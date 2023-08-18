15da87fbd8a7b39240a2826c95c06f44ba6b92b0
SaadSamir7 <132520150+SaadSamir7@users.noreply.github.com>
2023-08-18 05:05:45 +0300

    variadic_func

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,33 +3,22 @@
 #include <stdarg.h>
 
 /**
- *print_strings - a function that prints a string
- *@separator: separator between strings
- *@n: numbers of arguments
+ *print_numbers - prints numbers
+ *@separator: separator between numbers
+ *@n: numbers of args
  *Return: nothing
- */
-void print_strings(const char *separator, const unsigned int n, ...)
+ **/
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
+	va_list st;
 	unsigned int i;
 
-	char *p;
-
-	va_start(args, n);
+	va_start(st, n);
 
 	for (i = 0; i < n; i++)
 	{
-		p = va_arg(args, char *);
-		if (p == NULL)
-		{
-			printf("(nil)");
-		}
-		else
-		{
-			printf("%s", p);
-		}
-
-		if (i < n - 1)
+		printf("%d", va_arg(st, int));
+		if (i != n - 1)
 		{
 			if (separator)
 				printf("%s", separator);
@@ -37,5 +26,5 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 
 	printf("\n");
-	va_end(args);
+	va_end(st);
 }
